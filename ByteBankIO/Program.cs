351714f9f4acef0fd34e450a93002ec9eafbd825
Paulo Cesar Passos Pereira <paulo.passos1@gmail.com>
2025-05-26 07:52:13 -0300

    lendo arquivo de texto com FileStream
    - Como lidar com arquivos grandes e maiores que a RAM do computador, como filmes e vídeos;
    - O que são Streams, classes que nos ajudam a exibir uma sequência de bytes;
    - A usar o método Read() e buffers para ler arquivos dessa forma;
    - Unicode e encoding UTF entendendo como eles podem nos ajudar nesse processo de decodificação de um arquivo.
    - Este conteúdo foi útil para o seu aprendizado?

---
+++ ByteBankIO/Program.cs
@@ -0,0 +1,37 @@
+﻿using ByteBankIO;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var endereco = "contas.txt";
+        var numeroBytesLidos = -1;
+        var fluxoDoArquivo = new FileStream(endereco, FileMode.Open);
+        var buffer = new byte[1024];
+
+        numeroBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+      
+        while (numeroBytesLidos > 0)
+        {
+            EscreverBuffer(buffer);
+            numeroBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+        }
+
+        Console.ReadLine();
+    }
+
+    static void EscreverBuffer(byte[] buffer)
+    {
+        var utfe8 = new UTF8Encoding();
+
+        var texto = utfe8.GetString(buffer);
+        Console.Write(texto);
+
+        //foreach (var meuByte in buffer) 
+        //{
+        //    Console.Write(meuByte);
+        //    Console.Write(" ");
+        //}
+    }
+}
