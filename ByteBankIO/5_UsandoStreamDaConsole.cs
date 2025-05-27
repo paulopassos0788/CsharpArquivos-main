09b1a46db769707faf88b4dffa32c718c9255a7c
Paulo Cesar Passos Pereira <paulo.passos1@gmail.com>
2025-05-27 18:57:20 -0300

    stream da console
    - Como a console funciona com streams;
    - O método Console.OpenStandardInput() e como ele adquire o fluxo de entrada padrão;
    - Métodos auxiliares na classe File ajudam na criação, cópia, abertura, dentre outras ações referentes a um arquivo.

---
+++ ByteBankIO/5_UsandoStreamDaConsole.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBankIO;
+
+internal class _5_UsandoStreamDaConsole
+{
+    public static void UsarStreamDaConsole() 
+    {
+        using (var fluxoDeEntrada = Console.OpenStandardInput(1024))
+        using (var fs = new FileStream("entradaConsole.txt", FileMode.Create))
+        {
+            var buffer = new byte[1024];
+
+            while (true)
+            {
+                var bytesLidos = fluxoDeEntrada.Read(buffer, 0, 1024);
+                fs.Write(buffer, 0, bytesLidos);
+                fs.Flush(); // Garante que os dados sejam gravados no arquivo imediatamente
+
+                Console.WriteLine($"Bytes lidos: {bytesLidos}");
+            }
+        }
+        
+    }
+}
--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -9,8 +9,22 @@ class Program
         //_3_CriandoArquivo.CriandoArquivo();
         //_3_CriandoArquivo.CriarArquivoComWrite();
         //_3_CriandoArquivo.TestaEscrita();
-        _4_StreamBinario.EscritaBinaria();
-        _4_StreamBinario.LeituraBinaria();
+        //_4_StreamBinario.EscritaBinaria();
+        //_4_StreamBinario.LeituraBinaria();
+        //_5_UsandoStreamDaConsole.UsarStreamDaConsole();
+
+        var linhas = File.ReadAllLines("contas.txt");
+        Console.WriteLine(linhas.Length);
+
+        //foreach (var linha in linhas)
+        //{
+        //    Console.WriteLine(linha);
+        //}
+
+        var bytesArquivo = File.ReadAllBytes("contas.txt");
+        Console.WriteLine($"Arquivo contas.txt possui {bytesArquivo.Length} bytes");
+
+        File.WriteAllText("escrevendoComClasseFile.txt", "Testando File.WriteAllText");
 
         //var caminhoNovoArquivo = "TestaEscrita.txt";
 
